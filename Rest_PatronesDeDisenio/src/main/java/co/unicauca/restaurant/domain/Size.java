d351999552c33356f17c8a8c415c70c3aba5be7b
brabadi <74464904+brabadi@users.noreply.github.com>
2021-04-19 17:31:22 -0500

    Delete Rest_PatronesDeDisenio directory

--- Rest_PatronesDeDisenio/src/main/java/co/unicauca/restaurant/domain/Size.java
+++
@@ -1,9 +0,0 @@
-
-package co.unicauca.restaurant.domain;
-/**
-    * enum para crear los distintos tipos de taqmaño que tendran 
-    * los platos de la carta los cuales son:  Familiar, Doble, Personal respectivamente.
-    */
-public enum Size {
-    FAMILIAR, DOBLE , PERSONAL
-}
